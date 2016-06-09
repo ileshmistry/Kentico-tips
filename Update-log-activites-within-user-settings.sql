8a5463016662a34e9402f0c770560c4df72ecff7
ileshmistry <ilesh.m@mmtdigital.co.uk>
2016-06-09 22:26:10 +0100

    Apply comments to specific SQL

--- Update-log-activites-within-user-settings.sql
+++ Update-log-activites-within-user-settings.sql
@@ -12,6 +12,7 @@ WHERE [Email] LIKE '%@mmtdigital%'
 OR [Email] LIKE '%@kentico%'
 GO
 
+-- Identify what columns you may require to be shown/used
 SELECT
   [UserSettingsID],
   [UserSettingsUserID],
@@ -19,6 +20,7 @@ SELECT
 FROM [dbo].[CMS_UserSettings]
 GO
 
+--  Combine two tables (CMS_User and CMS_UserSettings) together to show you list of all users with the User Setting for Log activities
 SELECT
   u.[UserID],
   s.[UserSettingsUserID],
@@ -32,17 +34,7 @@ WHERE u.[Email] LIKE '%@mmtdigital%'
 OR [Email] LIKE '%@kentico%'
 ORDER BY u.[UserID] ASC
 
-SELECT
-  u.[UserID],
-  s.[UserSettingsUserID],
-  u.[FullName],
-  u.[Email],
-  s.[UserLogActivities]
-FROM [dbo].[CMS_User] u
-INNER JOIN [dbo].[CMS_UserSettings] s
-  ON u.[UserID] = s.[UserSettingsUserID]
-ORDER BY u.[UserID] ASC
-
+-- Change the User Setting for Log activities for the users to be set to false
 BEGIN TRAN Live
 UPDATE s
 SET s.[UserLogActivities] = 0
